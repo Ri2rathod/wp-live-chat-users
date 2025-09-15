fbdcab91af374c6e225a19d66e8eab8e844f6c57
Rathod Ritesh <rathodriteshrock@gmail.com>
2025-09-15 22:46:36 +0530

    refactor: Update REST API routes for improved authentication
    - Minor adjustments to REST API route definitions
    - Ensure consistent permission callback usage
    - Support for new API key authentication flow

--- wp-plugin/app/api/WPLCRestApiRoutes.php
+++ wp-plugin/app/api/WPLCRestApiRoutes.php
@@ -6,7 +6,8 @@ use WP_REST_Server;
 
 defined('ABSPATH') or die('Something went wrong');
 
-class WPLCRestApiRoutes {
+class WPLCRestApiRoutes
+{
 
     /**
      * @var WPLCRestApiRoutes
@@ -26,8 +27,9 @@ class WPLCRestApiRoutes {
     /**
      * Get singleton instance
      */
-    public static function instance() {
-        if ( ! isset( self::$instance ) && ! ( self::$instance instanceof WPLCRestApiRoutes ) ) {
+    public static function instance()
+    {
+        if (!isset(self::$instance) && !(self::$instance instanceof WPLCRestApiRoutes)) {
             self::$instance = new WPLCRestApiRoutes();
         }
 
@@ -37,7 +39,8 @@ class WPLCRestApiRoutes {
     /**
      * Initialize routes
      */
-    public function init() {
+    public function init()
+    {
         $this->controller = WPLCRestApiController::instance();
         add_action('rest_api_init', array($this, 'register_routes'));
     }
@@ -45,7 +48,8 @@ class WPLCRestApiRoutes {
     /**
      * Register all REST API routes
      */
-    public function register_routes() {
+    public function register_routes()
+    {
         $this->register_threads_routes();
         $this->register_messages_routes();
         $this->register_attachments_routes();
@@ -56,34 +60,37 @@ class WPLCRestApiRoutes {
     /**
      * Register threads endpoints
      */
-    private function register_threads_routes() {
+    private function register_threads_routes()
+    {
         register_rest_route($this->namespace, '/threads', array(
-            array(
-                'methods' => WP_REST_Server::READABLE,
-                'callback' => array($this->controller, 'get_threads'),
-                'permission_callback' => array($this->controller, 'check_user_permissions'),
-                'args' => array(
-                    'page' => array(
-                        'default' => 1,
-                        'validate_callback' => function($param) {
-                            return is_numeric($param) && $param > 0;
-                        }
-                    ),
-                    'per_page' => array(
-                        'default' => 20,
-                        'validate_callback' => function($param) {
-                            return is_numeric($param) && $param > 0 && $param <= 100;
-                        }
-                    ),
-                    'search' => array(
-                        'default' => '',
-                        'validate_callback' => function($param) {
-                            return is_string($param);
-                        },
-                        'sanitize_callback' => 'sanitize_text_field'
-                    )
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => array($this->controller, 'get_threads'),
+            'permission_callback' => array($this->controller, 'check_user_permissions'),
+            'args' => array(
+                'page' => array(
+                    'default' => 1,
+                    'validate_callback' => function ($param) {
+                        return is_numeric($param) && $param > 0;
+                    }
+                ),
+                'per_page' => array(
+                    'default' => 20,
+                    'validate_callback' => function ($param) {
+                        return is_numeric($param) && $param > 0 && $param <= 100;
+                    }
+                ),
+                'search' => array(
+                    'default' => '',
+                    'validate_callback' => function ($param) {
+                        return is_string($param);
+                    },
+                    'sanitize_callback' => 'sanitize_text_field'
                 )
-            ),
+            )
+        ));
+        register_rest_route(
+            $this->namespace,
+            '/threads',
             array(
                 'methods' => WP_REST_Server::CREATABLE,
                 'callback' => array($this->controller, 'create_thread'),
@@ -91,103 +98,103 @@ class WPLCRestApiRoutes {
                 'args' => array(
                     'type' => array(
                         'required' => true,
-                        'validate_callback' => function($param) {
+                        'validate_callback' => function ($param) {
                             return in_array($param, array('private', 'group'));
                         },
                         'sanitize_callback' => 'sanitize_text_field'
                     ),
                     'title' => array(
                         'required' => false,
-                        'validate_callback' => function($param) {
+                        'validate_callback' => function ($param) {
                             return is_string($param) && strlen($param) <= 255;
                         },
                         'sanitize_callback' => 'sanitize_text_field'
                     ),
                     'participants' => array(
                         'required' => false,
-                        'validate_callback' => function($param) {
+                        'validate_callback' => function ($param) {
                             return is_array($param) && !empty($param);
                         },
-                        'sanitize_callback' => function($param) {
+                        'sanitize_callback' => function ($param) {
                             return array_map('absint', $param);
                         }
                     )
                 )
             )
-        ));
+        );
     }
 
     /**
      * Register messages endpoints
      */
-    private function register_messages_routes() {
+    private function register_messages_routes()
+    {
         // Single thread messages endpoint
         register_rest_route($this->namespace, '/threads/(?P<thread_id>\d+)/messages', array(
-            array(
-                'methods' => WP_REST_Server::READABLE,
-                'callback' => array($this->controller, 'get_thread_messages'),
-                'permission_callback' => array($this->controller, 'check_thread_access_permissions'),
-                'args' => array(
-                    'thread_id' => array(
-                        'required' => true,
-                        'validate_callback' => function($param) {
-                            return is_numeric($param) && $param > 0;
-                        },
-                        'sanitize_callback' => 'absint'
-                    ),
-                    'before' => array(
-                        'default' => '',
-                        'validate_callback' => function($param) {
-                            return empty($param) || strtotime($param) !== false;
-                        },
-                        'sanitize_callback' => 'sanitize_text_field'
-                    ),
-                    'limit' => array(
-                        'default' => 50,
-                        'validate_callback' => function($param) {
-                            return is_numeric($param) && $param > 0 && $param <= 100;
-                        },
-                        'sanitize_callback' => 'absint'
-                    )
+            'methods' => WP_REST_Server::READABLE,
+            'callback' => array($this->controller, 'get_thread_messages'),
+            'permission_callback' => array($this->controller, 'check_thread_access_permissions'),
+            'args' => array(
+                'thread_id' => array(
+                    'required' => true,
+                    'validate_callback' => function ($param) {
+                        return is_numeric($param) && $param > 0;
+                    },
+                    'sanitize_callback' => 'absint'
+                ),
+                'before' => array(
+                    'default' => '',
+                    'validate_callback' => function ($param) {
+                        return empty($param) || strtotime($param) !== false;
+                    },
+                    'sanitize_callback' => 'sanitize_text_field'
+                ),
+                'limit' => array(
+                    'default' => 50,
+                    'validate_callback' => function ($param) {
+                        return is_numeric($param) && $param > 0 && $param <= 100;
+                    },
+                    'sanitize_callback' => 'absint'
                 )
-            ),
-            array(
-                'methods' => WP_REST_Server::CREATABLE,
-                'callback' => array($this->controller, 'send_message'),
-                'permission_callback' => array($this->controller, 'check_thread_access_permissions'),
-                'args' => array(
-                    'thread_id' => array(
-                        'required' => true,
-                        'validate_callback' => function($param) {
-                            return is_numeric($param) && $param > 0;
-                        },
-                        'sanitize_callback' => 'absint'
-                    ),
-                    'content' => array(
-                        'required' => true,
-                        'validate_callback' => function($param) {
-                            return is_string($param) && !empty(trim($param)) && strlen($param) <= 10000;
-                        },
-                        'sanitize_callback' => function($param) {
-                            return wp_kses_post(trim($param));
-                        }
-                    ),
-                    'content_type' => array(
-                        'default' => 'text/plain',
-                        'validate_callback' => function($param) {
-                            return in_array($param, array('text/plain', 'text/markdown', 'reaction', 'system'));
-                        },
-                        'sanitize_callback' => 'sanitize_text_field'
-                    ),
-                    'attachment_ids' => array(
-                        'default' => array(),
-                        'validate_callback' => function($param) {
-                            return is_array($param);
-                        },
-                        'sanitize_callback' => function($param) {
-                            return array_map('absint', $param);
-                        }
-                    )
+            )
+        ));
+        register_rest_route($this->namespace, '/threads/(?P<thread_id>\d+)', array(
+
+            'methods' => WP_REST_Server::CREATABLE,
+            'callback' => array($this->controller, 'send_message'),
+            'permission_callback' => array($this->controller, 'check_thread_access_permissions'),
+            'args' => array(
+                'thread_id' => array(
+                    'required' => true,
+                    'validate_callback' => function ($param) {
+                        return is_numeric($param) && $param > 0;
+                    },
+                    'sanitize_callback' => 'absint'
+                ),
+                'content' => array(
+                    'required' => true,
+                    'validate_callback' => function ($param) {
+                        return is_string($param) && !empty(trim($param)) && strlen($param) <= 10000;
+                    },
+                    'sanitize_callback' => function ($param) {
+                        return wp_kses_post(trim($param));
+                    }
+                ),
+                'content_type' => array(
+                    'default' => 'text/plain',
+                    'validate_callback' => function ($param) {
+                        return in_array($param, array('text/plain', 'text/markdown', 'reaction', 'system'));
+                    },
+                    'sanitize_callback' => 'sanitize_text_field'
+                ),
+                'attachment_ids' => array(
+                    'default' => array(),
+                    'validate_callback' => function ($param) {
+                        return is_array($param);
+                    },
+                    'sanitize_callback' => function ($param) {
+                        return array_map('absint', $param);
+                    }
                 )
             )
         ));
@@ -196,7 +203,8 @@ class WPLCRestApiRoutes {
     /**
      * Register attachments endpoints
      */
-    private function register_attachments_routes() {
+    private function register_attachments_routes()
+    {
         // File upload endpoint
         register_rest_route($this->namespace, '/attachments', array(
             'methods' => WP_REST_Server::CREATABLE,
@@ -214,7 +222,8 @@ class WPLCRestApiRoutes {
     /**
      * Register typing indicator endpoints
      */
-    private function register_typing_routes() {
+    private function register_typing_routes()
+    {
         // Typing indicator endpoint
         register_rest_route($this->namespace, '/threads/(?P<thread_id>\d+)/typing', array(
             'methods' => WP_REST_Server::CREATABLE,
@@ -223,17 +232,17 @@ class WPLCRestApiRoutes {
             'args' => array(
                 'thread_id' => array(
                     'required' => true,
-                    'validate_callback' => function($param) {
+                    'validate_callback' => function ($param) {
                         return is_numeric($param) && $param > 0;
                     },
                     'sanitize_callback' => 'absint'
                 ),
                 'is_typing' => array(
                     'required' => true,
-                    'validate_callback' => function($param) {
+                    'validate_callback' => function ($param) {
                         return is_bool($param) || in_array($param, array('true', 'false', '1', '0'));
                     },
-                    'sanitize_callback' => function($param) {
+                    'sanitize_callback' => function ($param) {
                         return filter_var($param, FILTER_VALIDATE_BOOLEAN);
                     }
                 )
@@ -248,7 +257,7 @@ class WPLCRestApiRoutes {
             'args' => array(
                 'thread_id' => array(
                     'required' => true,
-                    'validate_callback' => function($param) {
+                    'validate_callback' => function ($param) {
                         return is_numeric($param) && $param > 0;
                     },
                     'sanitize_callback' => 'absint'
@@ -260,7 +269,8 @@ class WPLCRestApiRoutes {
     /**
      * Register read receipts endpoints
      */
-    private function register_read_receipts_routes() {
+    private function register_read_receipts_routes()
+    {
         // Mark messages as read endpoint
         register_rest_route($this->namespace, '/threads/(?P<thread_id>\d+)/read', array(
             'methods' => WP_REST_Server::CREATABLE,
@@ -269,14 +279,14 @@ class WPLCRestApiRoutes {
             'args' => array(
                 'thread_id' => array(
                     'required' => true,
-                    'validate_callback' => function($param) {
+                    'validate_callback' => function ($param) {
                         return is_numeric($param) && $param > 0;
                     },
                     'sanitize_callback' => 'absint'
                 ),
                 'message_id' => array(
                     'required' => false,
-                    'validate_callback' => function($param) {
+                    'validate_callback' => function ($param) {
                         return is_numeric($param) && $param > 0;
                     },
                     'sanitize_callback' => 'absint'
@@ -292,7 +302,7 @@ class WPLCRestApiRoutes {
             'args' => array(
                 'message_id' => array(
                     'required' => true,
-                    'validate_callback' => function($param) {
+                    'validate_callback' => function ($param) {
                         return is_numeric($param) && $param > 0;
                     },
                     'sanitize_callback' => 'absint'
